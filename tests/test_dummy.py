8a994fdb2d1680fd94d4f4f717d18f7e87a41b7f
kim <audehdqlffk@naver.com>
2024-11-10 22:50:24 +0900

    Fix pylint warnings and update GitHub Actions workflow

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,4 +1,7 @@
 # tests/test_dummy.py
 
-def test_example():
-    assert True  # 항상 통과하는 테스트
+"""This is a dummy test module."""
+
+def test_sample():
+    """This is a sample test function."""
+    assert True
